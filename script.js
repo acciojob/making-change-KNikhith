a724ca0b9f8695e16bb8232bc76b41af2ea66ed5
KNikhith <nikhithkotluri@gmail.com>
2026-04-09 22:26:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let result={}
+	result.q = Math.floor(c / 25);
+	  c = c % 25;
+
+  result.d = Math.floor(c / 10);
+  c = c % 10;
+
+  result.n = Math.floor(c / 5);
+  c = c % 5;
+
+  result.p = c;
+
+	return result;
 };
 
 // Do not the change the code below
